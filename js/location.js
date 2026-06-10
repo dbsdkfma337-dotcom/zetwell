317f7fd20dd7d660215f8544476304ca3d54c603
Lee suyeon <suyeon@hanswell.com>
2026-06-10 15:30:23 +0900

    FOOTER 추가

--- js/location.js
+++ js/location.js
@@ -1,16 +1,16 @@
 kakao.maps.load(function () {
-  const container = document.getElementById("map");
+    const container = document.getElementById("map");
 
-  const options = {
-    center: new kakao.maps.LatLng(37.469977, 126.885614),
-    level: 3,
-  };
+    const options = {
+        center: new kakao.maps.LatLng(37.469977, 126.885614),
+        level: 3,
+    };
 
-  const map = new kakao.maps.Map(container, options);
+    const map = new kakao.maps.Map(container, options);
 
-  const marker = new kakao.maps.Marker({
-    position: new kakao.maps.LatLng(37.469977, 126.885614),
-  });
+    const marker = new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(37.469977, 126.885614),
+    });
 
-  marker.setMap(map);
+    marker.setMap(map);
 });
