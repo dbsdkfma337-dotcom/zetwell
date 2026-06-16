7f1fe815353e572a7e96eb93b290be4c50678180
Lee suyeon <suyeon@hanswell.com>
2026-06-16 14:51:43 +0900

    하단 고정 아이콘 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const header = document.querySelector("#header");
 const gnbArea = document.querySelector("#header .gnb-area");
 const subBg = document.querySelector("#header .sub-bg");
+const quickContact = document.getElementById("quickContact");
+const quickBtn = document.getElementById("quickBtn");
 
 gnbArea.addEventListener("mouseenter", () => {
     header.classList.add("open");
@@ -8,4 +10,8 @@ gnbArea.addEventListener("mouseenter", () => {
 
 header.addEventListener("mouseleave", () => {
     header.classList.remove("open");
+});
+
+quickBtn.addEventListener("click", function () {
+    quickContact.classList.toggle("active");
 });
