b111d7d4d67023f9c357cfde869a80b744845592
Lee suyeon <suyeon@hanswell.com>
2026-06-22 10:56:13 +0900

    헤더 변경

---
+++ js/hover.js
@@ -0,0 +1,54 @@
+const productVisualData = {
+    mobile: {
+        title: "NVIS 모니터",
+        bg: "./img/nvis-w22-datial5.png",
+        link: "./product/S-Series.html?series=n-series"
+    },
+    terminal: {
+        title: "W-Series",
+        bg: "./img/Zetstation-bg.png",
+        link: "./product/S-Series.html?series=w-series"
+    },
+    rfid: {
+        title: "E-Series",
+        bg: "./img/UK2000-bg.png",
+        link: "./product/S-Series.html?series=e-series"
+    },
+    tablet: {
+        title: "M-Series",
+        bg: "./img/E-seriesbg.png",
+        link: "./product/S-Series.html?series=m-series"
+    },
+    scanner: {
+        title: "43IG",
+        bg: "./img/43IG1.png",
+        link: "./product/table.html"
+    }
+};
+
+const visual = document.getElementById("productVisual");
+const title = document.getElementById("productTitle");
+const menuItems = document.querySelectorAll("#productMenu li");
+
+menuItems.forEach(item => {
+    const key = item.dataset.key;
+
+    item.addEventListener("mouseenter", () => {
+        const data = productVisualData[key];
+
+        visual.style.backgroundImage = `url(${data.bg})`;
+        title.textContent = data.title;
+
+        menuItems.forEach(el => el.classList.remove("active"));
+        item.classList.add("active");
+    });
+
+    item.addEventListener("click", () => {
+        location.href = productVisualData[key].link;
+    });
+});
+
+const defaultProduct = productVisualData.mobile;
+
+visual.style.backgroundImage = `url(${defaultProduct.bg})`;
+title.textContent = defaultProduct.title;
